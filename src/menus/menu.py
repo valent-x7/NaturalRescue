b57f0398206df89e4637504a64eaceb545322b3e
SolidFIE <adiaz106@ucol.mx>
2025-09-21 16:42:56 -0600

    Añadí los personajes en la pantalla del menu principal,  puse un título provisional en lo que decidimos cual vamos a usar, cambié la tipografía por una de 8 bit, modifiqué el tamaño de los botones y agregué un botón para salir del programa

--- src/menus/menu.py
+++ src/menus/menu.py
@@ -16,24 +16,59 @@ def draw_menu(screen, events):
     bg = pygame.transform.scale(bg, (main_settings.WINDOW_WIDTH, main_settings.WINDOW_HEIGHT))
     screen.blit(bg, (0,0))
 
+    # ? Fuentes de texto estilo 8 bit
+
+    # Fuente GRANDE para el título
+    fuente_titulo = pygame.font.Font("src/menus/fuentestexto/prstartk.ttf", 64)
+
+    # Fuente PEQUEÑA para los botones
+    fuente_botones = pygame.font.Font("src/menus/fuentestexto/prstartk.ttf", 24)
+
+    # ? Título del videojuego
+    title_text = fuente_titulo.render("Título provisional", True, (255, 255, 255))
+    text_rect = title_text.get_rect(center=(main_settings.WINDOW_WIDTH / 2, 100))
+    screen.blit(title_text, text_rect)
+
     # ? Botones del menu
-    play_btn = Button(screen, (0, 310), main_settings.FONT, 250, 50, 'Jugar', '#22d3ee', '#06b6d4')
-    tutorial_btn = Button(screen, (0, 380), main_settings.FONT, 250, 50, 'Tutorial', '#ef4444', '#dc2626')
-    settings_btn = Button(screen, (0, 450), main_settings.FONT, 250, 50, 'Settings', '#a8a29e', '#78716c')
+    play_btn = Button(screen, (main_settings.WINDOW_WIDTH // 2 - 150, 400), fuente_botones, 300, 90, 'Jugar', '#22d3ee', '#06b6d4')
+    tutorial_btn = Button(screen, (main_settings.WINDOW_WIDTH // 2 - 150, 520), fuente_botones, 300, 90, 'Tutorial', '#ef4444', '#dc2626')
+    settings_btn = Button(screen, (main_settings.WINDOW_WIDTH // 2 - 150, 640), fuente_botones, 300, 90, 'Settings', '#a8a29e', '#78716c')
+    salir_btn = Button(screen, (main_settings.WINDOW_WIDTH // 2 - 150, 760), fuente_botones, 300, 90, 'Salir', "#000000", "#050505")
 
     play_btn.draw()
     tutorial_btn.draw()
     settings_btn.draw()
+    salir_btn.draw()
+
+    # ? Personajes en la pantalla del menú
+    chango = pygame.image.load("./img/chango.png")
+    pinguino = pygame.image.load("./img/pinguino.png")
+
+    # ? Tamaño de la imagen
+    pinguino_rect = pinguino.get_rect()
+
+    # ? Calculamos una posición para el pingüino y asignamos una posición fija para el chango.
+    posicion_y = 400
+    pinguino_x = main_settings.WINDOW_WIDTH - pinguino_rect.width - 80
+    chango_x = 900
+
+    # ? Dibujamos el pingüino y el chango en las posiciones calculadas y asignadas previamente.
+    screen.blit(pinguino, (pinguino_x, posicion_y))
+    screen.blit(chango, (chango_x, posicion_y))
+
 
     # ? Recorremos los eventos
     for event in events:
         if play_btn.is_clicked(event):
-            return "PLAYING"
+            return "LEVEL_SELECT"
 
         elif tutorial_btn.is_clicked(event):
             return "TUTORIAL"
 
         elif settings_btn.is_clicked(event):
             return "SETTINGS"
+        
+        elif salir_btn.is_clicked(event):
+            return "SALIR"
 
     return "MENU"
