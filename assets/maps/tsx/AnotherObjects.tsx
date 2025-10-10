f4fdae4ed502eca0283763338e017c42ec441944
VacaMuu7 <valentinvacacipres@gmail.com>
2025-10-10 13:56:55 -0600

    Bug Tsx

--- assets/maps/tsx/AnotherObjects.tsx
+++ assets/maps/tsx/AnotherObjects.tsx
@@ -2,54 +2,54 @@
 <tileset version="1.10" tiledversion="1.11.2" name="AnotherObjects" tilewidth="32" tileheight="32" tilecount="17" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image source="../../../../JuegoRecursos/arbolobject.png" width="32" height="32"/>
+  <image source="../../images/objects/arbolobject.png" width="32" height="32"/>
  </tile>
  <tile id="1">
-  <image source="../../../../JuegoRecursos/arbolobject2.png" width="32" height="32"/>
+  <image source="../../images/objects/arbolobject2.png" width="32" height="32"/>
  </tile>
  <tile id="2">
-  <image source="../../../../JuegoRecursos/arbolobject3.png" width="32" height="32"/>
+  <image source="../../images/objects/arbolobject3.png" width="32" height="32"/>
  </tile>
  <tile id="3">
-  <image source="../../../../JuegoRecursos/arbolobject4.png" width="32" height="32"/>
+  <image source="../../images/objects/arbolobject4.png" width="32" height="32"/>
  </tile>
  <tile id="4">
-  <image source="../../../../JuegoRecursos/arbolobject5.png" width="32" height="32"/>
+  <image source="../../images/objects/arbolobject5.png" width="32" height="32"/>
  </tile>
  <tile id="5">
-  <image source="../../../../JuegoRecursos/arbolobject6.png" width="32" height="32"/>
+  <image source="../../images/objects/arbolobject6.png" width="32" height="32"/>
  </tile>
  <tile id="6">
-  <image source="../../../../JuegoRecursos/bushobject1.png" width="32" height="32"/>
+  <image source="../../images/objects/bushobject1.png" width="32" height="32"/>
  </tile>
  <tile id="7">
-  <image source="../../../../JuegoRecursos/bushobject2.png" width="32" height="32"/>
+  <image source="../../images/objects/bushobject2.png" width="32" height="32"/>
  </tile>
  <tile id="8">
-  <image source="../../../../JuegoRecursos/bushobject3.png" width="32" height="32"/>
+  <image source="../../images/objects/bushobject3.png" width="32" height="32"/>
  </tile>
  <tile id="9">
-  <image source="../../../../JuegoRecursos/rockobject1.png" width="32" height="32"/>
+  <image source="../../images/objects/rockobject1.png" width="32" height="32"/>
  </tile>
  <tile id="10">
-  <image source="../../../../JuegoRecursos/rockobject2.png" width="32" height="32"/>
+  <image source="../../images/objects/rockobject2.png" width="32" height="32"/>
  </tile>
  <tile id="11">
-  <image source="../../../../JuegoRecursos/rockobject3.png" width="32" height="32"/>
+  <image source="../../images/objects/rockobject3.png" width="32" height="32"/>
  </tile>
  <tile id="12">
-  <image source="../../../../JuegoRecursos/rockobject4.png" width="32" height="32"/>
+  <image source="../../images/objects/rockobject4.png" width="32" height="32"/>
  </tile>
  <tile id="13">
-  <image source="../../../../JuegoRecursos/signobjectdown.png" width="32" height="32"/>
+  <image source="../../images/objects/signobjectdown.png" width="32" height="32"/>
  </tile>
  <tile id="14">
-  <image source="../../../../JuegoRecursos/signobjectleft.png" width="32" height="32"/>
+  <image source="../../images/objects/signobjectleft.png" width="32" height="32"/>
  </tile>
  <tile id="15">
-  <image source="../../../../JuegoRecursos/signobjectright.png" width="32" height="32"/>
+  <image source="../../images/objects/signobjectright.png" width="32" height="32"/>
  </tile>
  <tile id="16">
-  <image source="../../../../JuegoRecursos/signobjectup.png" width="32" height="32"/>
+  <image source="../../images/objects/signobjectup.png" width="32" height="32"/>
  </tile>
 </tileset>
